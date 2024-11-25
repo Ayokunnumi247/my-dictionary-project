f0f5375afa278e95a490ce9dfb605d6b3d276495
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-11-25 19:13:56 +0000

    project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <footer>
+        The project was code {""}
+        <a href="https://github.com/Ayokunnumi247">Ayokunnumi Andu</a>
+        {""} and is {""}
+        <a href="https://github.com/Ayokunnumi247/my-dictionary-project">
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
