a87667dc4eaf1ee0c0c7ecd3a30bac16802a7fe1
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-12-17 20:21:05 +0000

    phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,12 +2,14 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <strong>Definition: </strong>
-      <p>{props.meaning.definition}</p>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+      </p>
+
       <strong>Example: </strong>
       <em>{props.meaning.example}</em>
 
--- src/Results.js
+++ src/Results.js
@@ -2,11 +2,11 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
         <h1>{props.results.word}</h1>
+        <p>{props.results.phonetic}</p>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
